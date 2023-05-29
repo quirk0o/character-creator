fd74c1d814f79933c3d33423905aab8f5352320e
Beata Obrok <beata.obrok@u2i.com>
2023-05-29 09:22:43 +0200

    feat: background

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React from 'react';
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
+import styles from './App.module.css';
+
+const Bg = () => <div className={styles.bg} />
+
 function App() {
   return (
-    <>
+    <div>
+      <Bg />
       <CharacterEditor />
       <Footer />
-    </>
+    </div>
   );
 }
 
